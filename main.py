2b181ec4694f52db9ddbab866b29a0513cbdf26b
TassioTenorio <121881174+TassioTenorio@users.noreply.github.com>
2023-12-14 22:23:19 -0300

    adding route for login page and criarconta page

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+lista_usuarios = ['Tassio', 'Nayara', 'Maria Eduarda', 'Thais']
+
 # decorators
 @app.route('/home')
-def hello_world():
+def home():
     return render_template('home.html')
 
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+
+@app.route('/lista_usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', login=login)
 
 
 if __name__ == '__main__':
